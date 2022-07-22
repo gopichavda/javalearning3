19dbacabedc38ac7843d30590b3b6889437378d1
Aarti chavda <aarti22rajput@gmail.com>
2022-07-22 22:09:47 +0100

    programme

---
+++ java3.java
@@ -0,0 +1,10 @@
+package jv3;
+
+public class java3 {
+
+    public static void main(String[] args) {
+        System.out.println("my");
+        System.out.println("first");
+        System.out.println("programme");
+    }
+}
